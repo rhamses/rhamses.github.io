478038c1c675e0abcf5077b79e0b82ad97b3c60e
Rhamses <rhamses.soares@gmail.com>
2024-04-23 09:26:19 -0300

    adding support for latest blog posts

---
+++ src/integrations/blog.ts
@@ -0,0 +1,16 @@
+export default async function () {
+  const blogPostsRaw = await fetch("https://rhams.es/blog/posts.json");
+  const blogPosts = await blogPostsRaw.json();
+  return blogPosts.posts
+    .map((post: any) => {
+      const { title, slug } = post;
+      const url = blogPosts.site + "/" + slug;
+      const image = blogPosts.site + "/assets/blog/" + slug + "/capa.webp";
+      https: return {
+        title,
+        url,
+        image,
+      };
+    })
+    .slice(0, 2);
+}
